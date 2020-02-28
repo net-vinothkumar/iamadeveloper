b66165aa91f5e156e6e791c185d5a831c7706ad5
vinothkumar <net.vinothkumar@gmail.com>
2020-02-28 05:17:28 +0100

    code everyday

---
+++ src/main/java/com/iamadeveloper/codeEveryday/CodeEverydayApplication.java
@@ -0,0 +1,13 @@
+package com.iamadeveloper.codeEveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEverydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEverydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamadeveloper/codeEveryday/CodeEverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamadeveloper.codeEveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeEverydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
